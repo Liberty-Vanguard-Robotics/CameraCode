a97bd752d78778aae6653cc1736aa8a30ce925c9
Silas-Collier <stcollier1@liberty.edu>
2025-02-13 19:28:43 -0500

    Code for rear Camera

---
+++ Camera_Code.py
@@ -0,0 +1,25 @@
+#Camera_Code
+import cv2
+
+# Captures Video change the # to the device id
+stream = cv2.VideoCapture(0)
+
+# Check for stream
+if not stream.isOpened():
+    print("No Stream")
+    exit()
+
+while(True):
+    ret, frame = stream.read()
+    # Check for end stream
+    if not ret:
+        print("No more stream")
+        break
+
+    # quits when q is pressed
+    cv2.imshow("Webcame", frame)
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+stream.release()
+cv2.destroyAllWindows()
